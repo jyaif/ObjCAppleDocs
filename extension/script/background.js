9a4463510229f19d370787910d499d7042953717
Matt Ao <m.ao@techsmith.com>
2016-11-29 13:17:34 -0500

    First iteration of extension, MVP

---
+++ extension/script/background.js
@@ -0,0 +1,23 @@
+var enabled = true;
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+        if (enabled && tab.url.indexOf("developer.apple.com/reference") != -1 && tab.url.indexOf("?language=objc") == -1) {
+            var objcURL = tab.url + "?language=objc";
+            chrome.tabs.update(tab.id, {url: objcURL});
+        }
+});
+
+
+chrome.browserAction.onClicked.addListener(function() {
+    if( enabled )
+    {
+        chrome.browserAction.setIcon({path:"objcdisabled.png"}); 
+        enabled = false;  
+    }
+    else
+    {
+        chrome.browserAction.setIcon({path:"objc.png"}); 
+        enabled = true;
+    }
+
+});
